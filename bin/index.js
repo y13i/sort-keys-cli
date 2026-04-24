a3a8154d614658707ceb68dbc2a38ac285618f11
Yoriki <7214104+y13i@users.noreply.github.com>
2026-04-25 01:00:15 +0900

    Migrate to ES module

--- bin/index.js
+++ bin/index.js
@@ -1,41 +1,49 @@
 #!/usr/bin/env node
-const { program } = require("commander");
-const { load, dump } = require("js-yaml");
-const { sortKeys } = require("@y13i/sort-keys");
+import { readFile } from "fs/promises";
+import { program } from "commander";
+import { load, dump } from "js-yaml";
+import { sortKeys } from "@y13i/sort-keys";
 
-const packageMetadata = require("../package.json");
+const { version } = JSON.parse(
+	await readFile(new URL("../package.json", import.meta.url)),
+);
 
 program
-  .name("sort-keys")
-  .usage("[options]")
-  .description(packageMetadata.description)
-  .version(packageMetadata.version)
-  .option("-d, --depth <number>", "how many times to recursively sort keys in a nested object or an array")
-  .option("-k, --prioritize-keys <keys...>", "prioritize the given keys in sorting")
-  .option("-p, --prioritize-primitives", "prioritize primitive values in sorting")
-  .option("-o, --output <json|yaml>", "output format", "yaml")
-  .parse();
+	.name("sort-keys")
+	.usage("[options]")
+	.description("Sort keys of the given YAML/JSON via STDIN.")
+	.version(version)
+	.option(
+		"-d, --depth <number>",
+		"how many times to recursively sort keys in a nested object or an array",
+	)
+	.option(
+		"-k, --prioritize-keys <keys...>",
+		"prioritize the given keys in sorting",
+	)
+	.option(
+		"-p, --prioritize-primitives",
+		"prioritize primitive values in sorting",
+	)
+	.option("-o, --output <json|yaml>", "output format", "yaml")
+	.parse();
 
 const option = program.opts();
 
-(async ()=>{
-  const buffers = [];
+const buffers = [];
+for await (const chunk of process.stdin) buffers.push(chunk);
 
-  for await (const chunk of process.stdin) buffers.push(chunk);
-  
-  const inputString = Buffer.concat(buffers).toString();
-  const data = load(inputString);
+const inputString = Buffer.concat(buffers).toString();
+const data = load(inputString);
 
-  const sortedData = sortKeys(data, {
-    depth: option.depth ? Number.parseInt(option.depth) : undefined,
+const sortedData = sortKeys(data, {
+	depth: option.depth ? Number.parseInt(option.depth) : undefined,
+	prioritize: {
+		keys: option.prioritizeKeys,
+		primitives: option.prioritizePrimitives,
+	},
+});
 
-    prioritize: {
-      keys: option.prioritizeKeys,
-      primitives: option.prioritizePrimitives,
-    },
-  });
-
-  const output = option.output === "json" ? JSON.stringify(sortedData) : dump(sortedData);
-
-  process.stdout.write(output);
-})();
+const output =
+	option.output === "json" ? JSON.stringify(sortedData) : dump(sortedData);
+process.stdout.write(output);
